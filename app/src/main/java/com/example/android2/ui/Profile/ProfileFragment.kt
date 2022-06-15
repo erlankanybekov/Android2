899a8c6ecdd593e88b3ea29c49b50f69cd90f2bd
erlankanybekov <erkanybekov@gmail.com>
2022-06-15 20:13:52 +0600

    add gallery fix

--- app/src/main/java/com/example/android2/ui/Profile/ProfileFragment.kt
+++ app/src/main/java/com/example/android2/ui/Profile/ProfileFragment.kt
@@ -15,92 +15,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.example.android2.databinding.ProfileFragmentBinding
 import java.lang.Exception
 
 class ProfileFragment : Fragment() {
 
     private lateinit var binding: ProfileFragmentBinding
-    var selectedImage : Uri?=null
-    var selectedBitmap:Bitmap?=null
-
-    companion object {
-        fun newInstance() = ProfileFragment()
-    }
-
-    private lateinit var viewModel: ProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = ProfileFragmentBinding.inflate(inflater,container,false)
+        binding = ProfileFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.imgProfile.setOnClickListener {
-            openGallery(it)
+            getContent.launch("image/*")
         }
     }
 
-     fun openGallery(view: View) {
-        activity?.let {
-            if (ContextCompat.checkSelfPermission(it.applicationContext,android.Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
-                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
-            }else{
-            val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            startActivityForResult(intent,2)
-            }
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+            Glide.with(binding.imgProfile).load(uri).centerCrop().into(binding.imgProfile)
         }
-
-
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode==1){
-            if (grantResults.size> 0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                startActivityForResult(intent,2)
-            }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == 2 && requestCode == Activity.RESULT_OK && data!=null){
-            selectedImage  = data.data
-        }
-        try {
-            context?.let {
-                if (selectedImage != null){
-                    if (Build.VERSION.SDK_INT>=28){
-                        val source = ImageDecoder.createSource(it.contentResolver,selectedImage!!)
-                        selectedBitmap = ImageDecoder.decodeBitmap(source)
-                        binding.imgProfile.setImageBitmap(selectedBitmap)
-                    } else{
-                        selectedBitmap = MediaStore.Images.Media.getBitmap(it.contentResolver,selectedImage)
-                        binding.imgProfile.setImageBitmap(selectedBitmap)
-                    }
-                }
-            }
-
-        }catch (e:Exception){
-            e.printStackTrace()
-        }
-        super.onActivityResult(requestCode, resultCode, data)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
